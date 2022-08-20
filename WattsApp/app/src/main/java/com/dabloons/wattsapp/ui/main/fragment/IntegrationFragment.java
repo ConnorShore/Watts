49749cc4abeceedd0c58a13a8ba718fce4d515ff
Zaid Abu-Hijleh <zaid.abu.hijleh@gmail.com>
2022-08-20 09:40:47 -0600

    structured some of the UI

--- WattsApp/app/src/main/java/com/dabloons/wattsapp/ui/main/fragment/IntegrationFragment.java
+++ WattsApp/app/src/main/java/com/dabloons/wattsapp/ui/main/fragment/IntegrationFragment.java
@@ -10,15 +10,15 @@ import android.view.ViewGroup;
 
 import com.dabloons.wattsapp.R;
 
-public class TestFragment extends Fragment {
+public class IntegrationFragment extends Fragment {
 
-    public TestFragment() {
+    public IntegrationFragment() {
         // Required empty public constructor
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        return inflater.inflate(R.layout.fragment_integration, container, false);
     }
 }

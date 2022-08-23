50ea3bdcfb3cf84663c1c0489144b0a7e8ec949c
Connor Shore <cjshore12@gmail.com>
2022-08-22 18:34:04 -0600

    file got deleted

---
+++ WattsApp/app/src/main/java/com/dabloons/wattsapp/ui/main/fragment/ConnectFragment.java
@@ -0,0 +1,29 @@
+package com.dabloons.wattsapp.ui.main.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.dabloons.wattsapp.R;
+import com.dabloons.wattsapp.manager.auth.PhillipsHueOAuthManager;
+
+public class ConnectFragment extends Fragment {
+
+    private final String LOG_TAG = "ConnectFragment";
+
+    private PhillipsHueOAuthManager phillipsHueOAuthManager = PhillipsHueOAuthManager.getInstance();
+
+    public ConnectFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View result = inflater.inflate(R.layout.fragment_connect, container, false);
+        result.findViewById(R.id.button_connect_phillips_hue).setOnClickListener(view -> {
+            phillipsHueOAuthManager.aquireAuthorizationCode(this.getActivity());
+        });
+        return result;
+    }
+}

d67da0a3e834ae27785c22f4661cb7a6a58c4e1d
Zaid Abu-Hijleh <zaid.abu.hijleh@gmail.com>
2022-08-25 14:05:45 -0600

    reworked the integration UI

---
+++ WattsApp/app/src/main/java/com/dabloons/wattsapp/ui/main/adapters/IntegrationAdapter.java
@@ -0,0 +1,81 @@
+package com.dabloons.wattsapp.ui.main.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dabloons.wattsapp.R;
+import com.dabloons.wattsapp.manager.LightManager;
+import com.dabloons.wattsapp.model.integration.IntegrationType;
+import com.dabloons.wattsapp.ui.room.adapters.LightAdapter;
+
+import java.util.ArrayList;
+
+public class IntegrationAdapter extends RecyclerView.Adapter<IntegrationAdapter.ViewHolder>
+{
+
+    private Context context;
+    public ArrayList<IntegrationType> integrationTypeList;
+
+    public IntegrationAdapter(Context context, ArrayList<IntegrationType> integrationTypeList) {
+        this.context = context;
+        this.integrationTypeList = integrationTypeList;
+    }
+
+    @NonNull
+    @Override
+    public IntegrationAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.integration_card_layout, parent, false);
+        return new IntegrationAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull IntegrationAdapter.ViewHolder holder, int position) {
+        IntegrationType type = integrationTypeList.get(position);
+        holder.integrationConnectStatus.setText("Connected");
+        holder.integrationConnectStatus.setTextColor(this.context.getColor(R.color.connected));
+        if(type == IntegrationType.PHILLIPS_HUE)
+        {
+            holder.integrationName.setText("Phillips Hue");
+
+        }
+        if(type == IntegrationType.NANOLEAF)
+        {
+            holder.integrationName.setText("Nanoleaf");
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(integrationTypeList == null)
+        {
+            return 0;
+        }
+        return integrationTypeList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView integrationName;
+        private TextView integrationConnectStatus;
+        private Button deleteIntegrationButton;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            integrationConnectStatus = itemView.findViewById(R.id.integrationConnectedStatus);
+            integrationName = itemView.findViewById(R.id.integrationName);
+            deleteIntegrationButton = itemView.findViewById(R.id.delete_integration_btn);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
--- WattsApp/app/src/main/java/com/dabloons/wattsapp/ui/main/fragment/ConnectFragment.java
+++ WattsApp/app/src/main/java/com/dabloons/wattsapp/ui/main/fragment/ConnectFragment.java
@@ -1,17 +1,34 @@
 package com.dabloons.wattsapp.ui.main.fragment;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
+import androidx.annotation.MenuRes;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.dabloons.wattsapp.R;
+import com.dabloons.wattsapp.WattsApplication;
 import com.dabloons.wattsapp.manager.LightManager;
+import com.dabloons.wattsapp.manager.UserManager;
 import com.dabloons.wattsapp.manager.auth.NanoleafAuthManager;
 import com.dabloons.wattsapp.manager.auth.PhillipsHueOAuthManager;
+import com.dabloons.wattsapp.model.integration.IntegrationType;
+import com.dabloons.wattsapp.ui.main.adapters.IntegrationAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import util.WattsCallback;
+import util.WattsCallbackStatus;
 
 public class ConnectFragment extends Fragment {
 
@@ -20,29 +37,73 @@ public class ConnectFragment extends Fragment {
     private PhillipsHueOAuthManager phillipsHueOAuthManager = PhillipsHueOAuthManager.getInstance();
     private NanoleafAuthManager nanoleafAuthManager = NanoleafAuthManager.getInstance();
 
+
+    private RecyclerView intergrationRV;
+    private IntegrationAdapter integrationAdapter;
+    private Button syncLightsBtn;
+
     public ConnectFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View result = inflater.inflate(R.layout.fragment_connect, container, false);
-
-        result.findViewById(R.id.button_connect_phillips_hue).setOnClickListener(view -> {
-            phillipsHueOAuthManager.aquireAuthorizationCode();
+        syncLightsBtn = result.findViewById(R.id.sync_lights_btn);
+        result.findViewById(R.id.connect_to_integration_Btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMenu(v, R.menu.popup_connect_menu);
+            }
         });
 
-        result.findViewById(R.id.button_connect_nanoleaf).setOnClickListener(view -> {
-            new AlertDialog.Builder(this.getActivity())
-                    .setMessage("Are you panels in connect mode? (Hold power button down for 5-7 seconds)")
-                    .setPositiveButton("Yes", (dialogInterface, i) ->
-                            nanoleafAuthManager.discoverNanoleafPanelsOnNetwork())
-                    .setNegativeButton("No", null)
-                    .show();
+        UserManager.getInstance().getUserIntegrations(new WattsCallback<List<IntegrationType>, Void>() {
+            @Override
+            public Void apply(List<IntegrationType> var, WattsCallbackStatus status) {
+                if(var.size() == 0)
+                {
+                    syncLightsBtn.setClickable(false);
+                }
+                integrationAdapter = new IntegrationAdapter(WattsApplication.getAppContext(), (ArrayList<IntegrationType>) var);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(WattsApplication.getAppContext(), LinearLayoutManager.VERTICAL, false);
+                intergrationRV = result.findViewById(R.id.integration_RV);
+                intergrationRV.setLayoutManager(linearLayoutManager);
+                intergrationRV.setAdapter(integrationAdapter);
+
+
+                return null;
+            }
         });
 
-        result.findViewById(R.id.button_sync_phillips_hue).setOnClickListener(view -> {
+        syncLightsBtn.setOnClickListener(view -> {
             LightManager.getInstance().syncLights();
         });
 
         return result;
+    }
+
+    private void showMenu(View v, @MenuRes int res)
+    {
+        PopupMenu popupMenu = new PopupMenu(getContext(), v);
+        popupMenu.getMenuInflater().inflate(res, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getTitle().equals("Phillips Hue"))
+                {
+                    phillipsHueOAuthManager.aquireAuthorizationCode();
+                }
+                if(item.getTitle().equals("NanoLeaf"))
+                {
+                    new AlertDialog.Builder(getActivity())
+                            .setMessage("Are you panels in connect mode? (Hold power button down for 5-7 seconds)")
+                            .setPositiveButton("Yes", (dialogInterface, i) ->
+                                    nanoleafAuthManager.discoverNanoleafPanelsOnNetwork())
+                            .setNegativeButton("No", null)
+                            .show();
+                }
+                return false;
+            }
+        });
+
+        popupMenu.show();
     }
 }

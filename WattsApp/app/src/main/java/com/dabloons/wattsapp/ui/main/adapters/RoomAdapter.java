1591a0bf16b0e0873b6410844552c778ee8c0cc4
Connor Shore <cjshore12@gmail.com>
2022-09-13 13:31:11 -0600

    Room light toggle will update glow

--- WattsApp/app/src/main/java/com/dabloons/wattsapp/ui/main/adapters/RoomAdapter.java
+++ WattsApp/app/src/main/java/com/dabloons/wattsapp/ui/main/adapters/RoomAdapter.java
@@ -1,17 +1,23 @@
 package com.dabloons.wattsapp.ui.main.adapters;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.dabloons.wattsapp.R;
+import com.dabloons.wattsapp.WattsApplication;
 import com.dabloons.wattsapp.manager.RoomManager;
 import com.dabloons.wattsapp.model.Light;
 import com.dabloons.wattsapp.model.Room;
@@ -46,19 +52,33 @@ public class RoomAdapter extends RecyclerView.Adapter<RoomAdapter.Viewholder>
         return new Viewholder(view);
     }
 
+    private void toggleBackgroundGlow(boolean on, MaterialCardView glowCard, int color) {
+        if(on) {
+            glowCard.setCardBackgroundColor(color);
+        }
+        else {
+            glowCard.setCardBackgroundColor(Color.TRANSPARENT);
+        }
+    }
+
     @Override
     public void onBindViewHolder(@NonNull Viewholder holder, int position)
     {
         Room room = mRoomModelArrayList.get(position);
         holder.roomName.setText(room.getName());
+        holder.glowCard.setCardBackgroundColor(Color.TRANSPARENT);
         holder.roomSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if(isChecked) {
+                int color = 0xFFFF5722; // todo: set to average color of all lights that will be on
+                toggleBackgroundGlow(true, holder.glowCard, color);
+
                 RoomManager.getInstance().turnOnRoomLights(room, (var, status) -> {
                     new Handler(Looper.getMainLooper()).post(() -> {
                         if (status.success)
                             UIMessageUtil.showShortToastMessage(buttonView.getContext(), "Turned on lights for room: " + room.getName());
                         else
                             UIMessageUtil.showShortToastMessage(buttonView.getContext(), "Failed to turn on lights for room: " + room.getName());
+
                     });
 
                     return null;
@@ -66,6 +86,7 @@ public class RoomAdapter extends RecyclerView.Adapter<RoomAdapter.Viewholder>
             }
             else
             {
+                toggleBackgroundGlow(false, holder.glowCard, Color.TRANSPARENT);
                 RoomManager.getInstance().turnOffRoomLights(room, (var, status) -> {
                     new Handler(Looper.getMainLooper()).post(() -> {
                         if (status.success)
@@ -102,12 +123,15 @@ public class RoomAdapter extends RecyclerView.Adapter<RoomAdapter.Viewholder>
         private TextView roomName;
         private SwitchMaterial roomSwitch;
         private MaterialCardView selectableCard;
+        private MaterialCardView glowCard;
 
         public Viewholder(@NonNull View itemView) {
             super(itemView);
             roomName = itemView.findViewById(R.id.roomName);
             roomSwitch = itemView.findViewById(R.id.roomSwitch);
             selectableCard = itemView.findViewById(R.id.roomSelectableCard);
+            glowCard = itemView.findViewById(R.id.roomGlowCard);
+
             selectableCard.setOnClickListener(this);
             itemView.setOnClickListener(this);
         }
